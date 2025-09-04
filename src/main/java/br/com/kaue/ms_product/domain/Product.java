1da57305d57570ead18832d66f64f337e7f1f9a7
KaueB15 <kauecardozo3@gmail.com>
2025-09-04 13:57:08 -0300

    feat: product entity

---
+++ src/main/java/br/com/kaue/ms_product/domain/Product.java
@@ -0,0 +1,31 @@
+package br.com.kaue.ms_product.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String maker;
+
+}
